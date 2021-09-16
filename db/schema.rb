0377b4581170423313015719bba45c9a16d11672
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2021-09-16 23:13:51 +0700

    initial commit for homework_class5 branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_071514) do
+ActiveRecord::Schema.define(version: 2021_09_16_161219) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "msg"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_09_16_071514) do
     t.string "postal_code"
   end
 
+  add_foreign_key "posts", "users"
 end
